5b17ff6ae39543c2e8b6c070f46586187a8587ed
Clo <35677277+Clost@users.noreply.github.com>
2022-05-17 09:30:42 -0500

    change files

---
+++ quizw4d1/index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "view"));
+
+app.use(express.urlencoded({ extended: true }));
+
+let data = ["Mangue","Citron", "Orange", "Banane"];
+
+app.get("/", (req, res) => {
+
+    let list = "<a href='/add'>Text</a><br/><ul>"
+
+    data.forEach(d => list +=`<li>${d}</li>`)
+
+    list += "</ul>"
+
+    res.send(list);
+
+  });
+
+  app.get("/add", (req, res) => {
+
+    res.send(`
+  
+          <!DOCTYPE html>
+  
+              <html lang="en">
+  
+              <head>
+  
+                  <meta charset="UTF-8">
+  
+                  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+                  <title>Quizw4d1</title>
+  
+              </head>
+  
+              <body>
+  
+                  <form method="POST" action="/add">
+  
+                     <label>Name</label>
+  
+                     <input type="text" name="text" />
+  
+                     <input type="submit" id="submit" value="Submit Text">
+  
+                  </form>
+  
+              </body>
+  
+          </html>
+  
+      `);
+  
+  });
+  
+  
+  
+  app.post("/add", (req, res) => {
+  
+    const { text } = req.body;
+  
+    let copyData = [...data];
+  
+    copyData.push(text);
+  
+    data = copyData;
+  
+    res.redirect('/');
+  
+  });
+  
+  
+  
+  app.listen(3000);
